2cf9df3cf97b286b83aa1ce4fad4efd16205d860
Volodymyr Petryna <volodymyr.petryna@nordstrom.com>
2023-10-21 03:52:02 +0300

    add schedules api

--- src/main/java/com/example/numo/controllers/ControllerAPI.java
+++ src/main/java/com/example/numo/controllers/ControllerAPI.java
@@ -7,4 +7,6 @@ public class ControllerAPI {
     public static final String CONTROLLER_SPECIFIC_REQUEST = "/{id}";
     public static final String CONTROLLER_GENERAL_REQUEST = "";
     public static final String GROUP_CONTROLLER = "/api/v1/groups";
+
+    public static final String SCHEDULE_CONTROLLER = "/api/v1/schedules";
 }
