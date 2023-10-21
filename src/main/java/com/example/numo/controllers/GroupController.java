7f39cbe9ca786babdcf346de247405a3d0d547de
Volodymyr Petryna <volodymyr.petryna@nordstrom.com>
2023-10-21 12:05:29 +0300

    add cors

--- src/main/java/com/example/numo/controllers/GroupController.java
+++ src/main/java/com/example/numo/controllers/GroupController.java
@@ -5,23 +5,15 @@ import com.example.numo.entities.elastic.Group;
 import com.example.numo.services.GroupService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(ControllerAPI.GROUP_CONTROLLER)
 @RequiredArgsConstructor
